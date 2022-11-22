06f6dd99d5215ec299083d4c9d8286a8ed03856e
mccl <mccl@MacBook-Pro-48.local>
2022-11-22 10:38:32 -0300

    adding xcode project

---
+++ QatarChallenge/QatarChallenge Watch App/QatarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QatarChallengeApp.swift
+//  QatarChallenge Watch App
+//
+//  Created by mccl on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct QatarChallenge_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
