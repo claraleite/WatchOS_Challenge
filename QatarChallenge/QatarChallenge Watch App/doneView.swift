2b39e79ce909fd81888aa9d22bbfa21c6c2bad0d
Natália Pessoa de Azevedo Albuquerque <npaa@192.168.1.12>
2022-11-24 09:23:26 -0300

    feat(feature/doneView): created doneView

---
+++ QatarChallenge/QatarChallenge Watch App/doneView.swift
@@ -0,0 +1,34 @@
+//
+//  doneView.swift
+//  QatarChallenge Watch App
+//
+//  Created by Natália Pessoa de Azevedo Albuquerque on 24/11/22.
+//
+
+import SwiftUI
+
+struct doneView: View {
+    var body: some View {
+        ZStack{
+            Image("party")
+                .opacity(0.2)
+            
+            VStack{
+                Image("trophy")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(6.0)
+                
+                Text("Você conseguiu!")
+                    .font(.system(size: 14))
+                    .fontWeight(.semibold)
+            }//:VSTACK
+        }//:ZSTACK
+    }
+}
+
+struct doneView_Previews: PreviewProvider {
+    static var previews: some View {
+        doneView()
+    }
+}
