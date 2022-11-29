8ed82d162f3e405050c8ffc64d685394ea43ad28
mccl <mccl@MacBook-Pro-48.local>
2022-11-29 11:39:32 -0300

    feat(feat/timer-play): changed timer type to play and pause

---
+++ QatarChallenge/QatarChallenge Watch App/StopWatchManager.swift
@@ -0,0 +1,41 @@
+//
+//  StopWatchManager.swift
+//  QatarChallenge Watch App
+//
+//  Created by mccl on 29/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+class StopWatchManager: ObservableObject {
+    
+    enum stopWatchMode {
+        case running
+        case stopped
+        case paused
+    }
+    @Published var mode: stopWatchMode = .stopped
+    
+    @Published var secondsElapsed = 30
+    var timer = Timer()
+    
+    func start() {
+        mode = .running
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.secondsElapsed -= 1
+            
+        }
+    }
+    
+    func pause() {
+        timer.invalidate()
+        mode = .paused
+    }
+    
+    func stop() {
+        timer.invalidate()
+        secondsElapsed = 30
+        mode = .stopped
+    }
+}
