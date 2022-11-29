d5644dbe9aec5ba79d24531209e530d72cc43884
Cecília <cecilia@MacBook-Pro-81.local>
2022-11-29 09:02:35 -0300

    delay doneView

--- QatarChallenge/QatarChallenge Watch App/doneView.swift
+++ QatarChallenge/QatarChallenge Watch App/doneView.swift
@@ -8,25 +8,54 @@
 import SwiftUI
 
 struct doneView: View {
+    
+    @State var pushNewView: Bool = false
+    
     var body: some View {
-        ZStack{
-            Image("party")
-                .opacity(0.2)
+        
+        NavigationView {
             
-            VStack{
-                Image("trophy")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: WKInterfaceDevice.current().screenBounds.size.width * 0.70, height: WKInterfaceDevice.current().screenBounds.size.height * 0.50)
-                    .padding(WKInterfaceDevice.current().screenBounds.size.height * 0.06)
+            ZStack{
                 
-                Text("Você conseguiu!")
-                    .font(.system(size: 15))
-                    .fontWeight(.semibold)
-            }//:VSTACK
-        }//:ZSTACK
+                NavigationLink(destination: concludedHomeView().navigationBarBackButtonHidden(true), isActive: $pushNewView) {
+                    EmptyView()
+                    
+                }.navigationTitle("")
+                    .toolbar(.hidden)
+                    .buttonStyle(.plain)
+                
+                Image("party")
+                    .opacity(0.2)
+                
+                VStack{
+                    Image("trophy")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: WKInterfaceDevice.current().screenBounds.size.width * 0.70, height: WKInterfaceDevice.current().screenBounds.size.height * 0.50)
+                        .padding(WKInterfaceDevice.current().screenBounds.size.height * 0.06)
+                    
+                    Text("Você conseguiu!")
+                        .font(.system(size: 15))
+                        .fontWeight(.semibold)
+                }
+            }
+        }//navView
+        
+        .onAppear {
+            
+            DispatchQueue.main.asyncAfter(deadline: .now()+3.0) {
+                self.pushNewView.toggle()
+            }
+            
+            //                    Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+            //                        pushNewView = true
+            //                    }
+        }
+        
+        
     }
 }
+
 
 struct doneView_Previews: PreviewProvider {
     static var previews: some View {
